f9d3dcf60f9a186aaef635167325d174047f6123
JM <jtmcn.dev@gmail.com>
2023-07-17 16:02:01 -0700

    add python commands to update-script

--- update.sh
+++ update.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
 
-# Update the repository with the latest changes
-git pull --quiet -r --autostash origin main
+git switch --quiet main
+
+git fetch --quiet
+
+if ! git diff --quiet origin/main main; then
+
+    git pull --quiet -r --autostash origin main
+
+    python3 -m venv .venv
+    source .venv/bin/activate 
+    python3 -m pip install --upgrade pip
+    python3 -m pip install -r requirements.txt
+    python3 -m pip install . 
+    echo "Updated"
+else
+    echo "No changes"
+
+fi
