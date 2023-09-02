eb52842b7888cc58f7dff9decb15c496d5abc259
JM <jtmcn.dev@gmail.com>
2023-09-01 18:17:47 -0700

    remove dbt packages

--- dbt_packages/codegen/integration_tests/macros/integer_type_value.sql
+++
@@ -1,9 +0,0 @@
-{%- macro integer_type_value() -%}
-{%- if target.type == "snowflake" -%}
-NUMBER(38,0)
-{%- elif target.type == "bigquery" -%}
-INT64
-{%- else -%}
-INTEGER
-{%- endif -%}
-{%- endmacro -%}
