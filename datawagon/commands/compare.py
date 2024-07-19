7844f73d77ef771ffcb062e66fa9cd222f6c05a8
JM <jtmcn.dev@gmail.com>
2024-07-19 10:28:17 -0700

    remove postgres libraries and workflows

--- datawagon/commands/compare.py
+++ datawagon/commands/compare.py
@@ -4,72 +4,12 @@ import click
 import pandas as pd
 from tabulate import tabulate
 
-from datawagon.commands.files_in_database import files_in_database
 from datawagon.commands.files_in_local_fs import files_in_local_fs
 from datawagon.commands.files_in_storage import files_in_storage
 from datawagon.objects.current_table_data import CurrentDestinationData
 from datawagon.objects.file_utils import FileUtils
 from datawagon.objects.managed_file_metadata import ManagedFileMetadata
 from datawagon.objects.managed_file_scanner import ManagedFilesToDatabase
-
-
-@click.command()
-@click.pass_context
-def compare_local_files_to_postgres(ctx: click.Context) -> List[ManagedFilesToDatabase]:
-    """Compare files in source directory to files in database."""
-
-    matched_files: List[ManagedFilesToDatabase] = ctx.invoke(files_in_local_fs)
-    current_database_files: List[CurrentDestinationData] = ctx.invoke(files_in_database)
-
-    csv_file_infos: List[ManagedFileMetadata] = [
-        file_info for src in matched_files for file_info in src.files
-    ]
-
-    file_diff_display_df = _file_diff(csv_file_infos, current_database_files)
-
-    click.echo(nl=True)
-    if len(file_diff_display_df) > 0:
-        click.secho(
-            tabulate(
-                # type ignore because tabulate does support pandas dataframes
-                file_diff_display_df,  # type: ignore
-                headers=["Table", "DB File Count", "Local File Count"],
-                tablefmt="simple",
-                showindex=False,
-                numalign="right",
-                intfmt=",",
-            )
-        )
-    else:
-        click.secho("No tables found.", fg="red")
-        ctx.abort()
-
-    new_files = _net_new_files(matched_files, current_database_files)
-
-    new_csv_file_infos: List[ManagedFileMetadata] = [
-        file_info for src in new_files for file_info in src.files
-    ]
-
-    new_file_count = len(new_csv_file_infos)
-
-    click.echo(nl=True)
-    click.echo(nl=True)
-
-    if new_file_count == 0:
-        click.secho("No new files found.", fg="yellow")
-    else:
-        display_limit = 10
-        i = 0
-        click.secho(f"Found {new_file_count} new files:", fg="green")
-        for file in new_csv_file_infos:
-            if i >= display_limit:
-                break
-            click.secho(f"{file.file_name}")
-            i += 1
-        if new_file_count > display_limit:
-            click.secho(f"...including {new_file_count - display_limit} more.")
-
-    return new_files
 
 
 @click.command()
--- datawagon/main.py
+++ datawagon/main.py
@@ -1,7 +1,4 @@
 import importlib.metadata
-import signal
-import subprocess
-import sys
 from pathlib import Path
 
 import click
@@ -9,21 +6,12 @@ import toml
 from dotenv import find_dotenv, load_dotenv
 from hologram import ValidationError
 
-from datawagon.commands.compare import (
-    compare_local_files_to_bucket,
-    compare_local_files_to_postgres,
-)
+from datawagon.commands.compare import compare_local_files_to_bucket
 from datawagon.commands.file_zip_to_gzip import file_zip_to_gzip
-from datawagon.commands.files_in_database import files_in_database
 from datawagon.commands.files_in_local_fs import files_in_local_fs
 from datawagon.commands.files_in_storage import files_in_storage
-from datawagon.commands.import_all_csv import import_all_csv
-from datawagon.commands.import_single_csv import import_selected_csv
-from datawagon.commands.reset_database import reset_database
 from datawagon.commands.upload_to_storage import upload_all_gzip_csv
-from datawagon.database.postgres_database_manager import PostgresDatabaseManager
 from datawagon.objects.app_config import AppConfig
-from datawagon.objects.parameter_validator import ParameterValidator
 from datawagon.objects.source_config import SourceConfig
 
 
@@ -64,10 +52,10 @@ def cli(
     gcs_project_id: str,
     gcs_bucket: str,
 ) -> None:
-    if not ParameterValidator(
-        db_url, db_schema, csv_source_dir, csv_source_config
-    ).are_valid_parameters:
-        ctx.abort()
+    # if not ParameterValidator(
+    #     db_url, db_schema, csv_source_dir, csv_source_config
+    # ).are_valid_parameters:
+    #     ctx.abort()
 
     # TODO: fix error handling, this is not working
     # load config from toml file
@@ -90,52 +78,22 @@ def cli(
         db_url=db_url,
         gcs_project_id=gcs_project_id,
         gcs_bucket=gcs_bucket,
-        # bucket_storage_url=bucket_storage_url
     )
-
-    db_manager = PostgresDatabaseManager(app_config)
-
-    # if on mac, prevent computer from sleeping (display, system, disk)
-    if "darwin" in sys.platform:
-        proc = subprocess.Popen(["caffeinate", "-dim"])
-
-    ctx.obj["DB_CONNECTION"] = db_manager
-
-    is_valid_db = check_db_connection(db_manager=db_manager)
-    is_valid_schema = (
-        check_schema(db_manager=db_manager, schema_name=db_schema)
-        if is_valid_db
-        else False
-    )
-
-    if not is_valid_schema:
-        ctx.abort()
-
-    if is_valid_db and is_valid_schema:
-        db_manager.create_log_table()
 
     ctx.obj["CONFIG"] = app_config
     ctx.obj["GLOBAL"] = {}
 
-    def on_exit() -> None:
-        if is_valid_db:
-            db_manager.close()
-        if proc:
-            proc.send_signal(signal.SIGTERM)
+    # def on_exit() -> None:
+    #     if proc:
+    #         proc.send_signal(signal.SIGTERM)
 
-    ctx.call_on_close(on_exit)
+    # ctx.call_on_close(on_exit)
 
 
-cli.add_command(reset_database)
-cli.add_command(files_in_database)
 cli.add_command(files_in_local_fs)
-cli.add_command(compare_local_files_to_postgres)
 cli.add_command(compare_local_files_to_bucket)
 cli.add_command(upload_all_gzip_csv)
 cli.add_command(file_zip_to_gzip)
-cli.add_command(import_all_csv)
-cli.add_command(import_selected_csv)
-cli.add_command(reset_database)
 cli.add_command(files_in_storage)
 
 
@@ -150,51 +108,6 @@ def start_cli() -> click.Group:
     click.echo(nl=True)
 
     return cli(obj={})  # type: ignore
-
-
-def check_db_connection(db_manager: PostgresDatabaseManager) -> bool:
-    """Test the connection to the database."""
-
-    if db_manager.is_valid_connection:
-        click.secho("Successfully connected to the database.", fg="green")
-        click.echo(nl=True)
-        return True
-    else:
-        click.secho("Failed to connect to the database.", fg="red")
-        click.echo(nl=True)
-        return False
-
-
-def check_schema(db_manager: PostgresDatabaseManager, schema_name: str) -> bool:
-    """Check if the schema exists and prompt to create if it does not."""
-
-    # This will try to create schema if it does not exist
-    if not ensure_schema_exists(db_manager, schema_name):
-        click.secho(f"Schema '{schema_name}' must exist. Exiting.", fg="red")
-        click.echo(nl=True)
-        return False
-
-    click.echo(nl=True)
-    click.secho(f"'{schema_name}' is valid schema.", fg="green")
-    click.echo(nl=True)
-    return True
-
-
-def ensure_schema_exists(db_manager: PostgresDatabaseManager, schema_name: str) -> bool:
-    if not db_manager.check_schema():
-        click.secho(f"Schema '{schema_name}' does not exist in the database.", fg="red")
-        if click.confirm("Create the schema?"):
-            db_manager.ensure_schema_exists()
-            if db_manager.check_schema():
-                click.secho(f"Schema '{schema_name}' created.", fg="green")
-                return True
-            else:
-                click.secho("Schema creation failed.", fg="red")
-                return False
-        else:
-            return False
-    else:
-        return True
 
 
 if __name__ == "__main__":
